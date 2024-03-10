4260b951cd1d473a96222fee921531fd9c27e6c8
Jeong Seongwoo <jeongseongwoo83@daum.net>
2024-03-10 20:02:44 +0900

    Initial commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wHHJkcYA6EKb2J6cpOxTMx0iPaJqT4VcTSdmRpkMrIU=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Navigation from '../components/navigation';
+export default function Page(){
+    return (<div>
+        <Navigation />
+        <h1>Hello</h1>
+    </div>)
+}
